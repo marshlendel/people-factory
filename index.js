3dc02fe6bfd60de0fabef95c0029b54d41b2b532
Marshall Brown <marshallendel@gmail.com>
2021-10-14 10:37:31 -0400

    create elements for inpput values and append. my version

--- index.js
+++ index.js
@@ -6,18 +6,20 @@ personForm.addEventListener("submit", handleSubmit);
 
 function handleSubmit(e) {
   e.preventDefault();
-  const nameValue = e.target.personName.value
-  const name = document.createElement("em")
-  name.innerText = nameValue
-  detailsDiv.appendChild(name)
-    personName.value = ""
+  const name = e.target.personName.value
+  const favColor = e.target.favColor.value
+  const age = e.target.age.value
+  const liOne = document.createElement("li")
+  detailsDiv.firstElementChild.appendChild(liOne)
+  const liTwo = document.createElement("li")
+  detailsDiv.firstElementChild.appendChild(liTwo)
+  const liThree = document.createElement("li")
+  detailsDiv.firstElementChild.appendChild(liThree)
+  liOne.innerText = name
+  liTwo.innerHTML = `
+  Favorite Color
+  <div style="background-color: ${favColor}; width: 100px; height: 100px;"></div>
+  `
+  liThree.innerText = age
 }
 
-
-//Add another input to the form
-//Use the values from both inputs in the h1
-
-//Add an empty paragraph to the page. 
-//Use the form values to do something with that paragraph
-
-//Change the appearance of the paragraph based on a value from the form
