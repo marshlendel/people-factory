da8f4c1ef2989c2f5e90dbc80f9c52a3d52bc75d
Marshall Brown <marshallendel@gmail.com>
2021-10-24 20:02:55 -0400

    Organize code in object

--- index.js
+++ index.js
@@ -1,48 +1,48 @@
-const personForm = document.querySelector("form");
-const heading = document.querySelector("h1")
-const detailsDiv = document.querySelector("#details")
+const PeopleFactory = {
+  renderColor(color) {
+    const colorDiv = document.createElement("div");
+    colorDiv.style.backgroundColor = color;
+    colorDiv.style.height = "50px";
+    colorDiv.style.width = "100px";
+    return colorDiv.outerHTML;
+  },
+  renderListItem(fieldName, value) {
+    const dt = document.createElement("dt");
+    const dd = document.createElement("dd");
+    const li = document.createElement("li");
+    dt.innerText = fieldName;
+    dd.innerHTML = value;
+    li.appendChild(dt);
+    li.appendChild(dd);
+    return li;
+  },
+  renderList(personData) {
+    const list = document.createElement("dl");
+    for (prop in personData) {
+      list.appendChild(this.renderListItem(prop, personData[prop]));
+    }
+    return list;
+  },
+  handleSubmit(e) {
+    e.preventDefault();
+    const name = e.target.personName.value;
+    const favColor = e.target.favColor.value;
+    const age = e.target.age.value;
 
-personForm.addEventListener("submit", handleSubmit);
+    const person = {
+      name: name,
+      favColor: this.renderColor(favColor),
+      age: age,
+    };
 
-function renderColor(color) {
-  const colorDiv = document.createElement("div")
-  colorDiv.style.backgroundColor = color
-  colorDiv.style.height = "50px"
-  colorDiv.style.width = "100px"
-  return colorDiv.outerHTML
-}
+    const detailsDiv = document.querySelector("#details");
+    detailsDiv.appendChild(this.renderList(person));
+  },
+  init(formName) {
+    const form = document.querySelector(formName);
+    form.addEventListener("submit", this.handleSubmit.bind(this));
+  },
+};
 
-function renderListItem(fieldName, value) {
-  const dt = document.createElement("dt")
-  const dd = document.createElement("dd")
-  const li = document.createElement("li")
-  dt.innerText = fieldName
-  dd.innerHTML = value
-  li.appendChild(dt)
-  li.appendChild(dd)
-  return li
-}
-
-function renderList(personData) {
-  const list = document.createElement("dl")
-  for (prop in personData) {
-    list.appendChild(renderListItem(prop, personData[prop]))
-  }
-  return list
-}
-
-function handleSubmit(e) {
-  e.preventDefault();
-  const name = this.personName.value
-  const favColor = this.favColor.value
-  const age = this.age.value
-
-  const person = {
-    name: name,
-    favColor: renderColor(favColor),
-    age: age,
-  }
-
-  detailsDiv.appendChild(renderList(person))
-}
-
+//Pass in the selector for your form
+PeopleFactory.init("form");
